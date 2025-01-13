3c788aa9e90c13bdb51a319411dea791fe2d3d6d
HR0110 <hrisiraya@abv.bg>
2025-01-13 21:56:32 +0200

    version update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ name 'HRStorages'
 author 'HRScripts Development'
 description 'Storage system'
 repository 'https://github.com/HRScripts/HRStorages'
-version '1.5.7'
+version '1.6.0'
 
 shared_script '@HRLib/import.lua'
 
